6b98111b514035a8c39eda1fd4b0157da94c021b
Pos <possesion193@gmail.com>
2019-10-27 17:20:09 +0300

    'timer'

--- script.js
+++ script.js
@@ -32,5 +32,49 @@ let tab = document.querySelectorAll('.info-header-tab'),
                 }
             }
         }
+        //Timer
     });
+   
+    let deadline ='2019-11-15';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60))); // same as divide
+        
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            } else {
+                minutes = minutes;
+            }
+    
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <=0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('timer', deadline);
 });
+
